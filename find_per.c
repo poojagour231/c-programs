76206f16533d0cee1ebd88c9dea6744671bb9fac
Pooja gour <poojagour231@gmail.com>
2025-07-07 15:07:09 +0530

    Create find_per.c

---
+++ find_per.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ float m1=100;
+ float m2=97;
+ float m3=92;
+ float m4=90;
+ float m5=86;
+ float p=(m1+m2+m3+m4+m5)/5;
+ printf("percantage of %f",p);
+
+    return 0;
+}
