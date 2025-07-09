d0227ecaa7a059e81ce67c4b7fbb4183abeb5491
Pooja gour <poojagour231@gmail.com>
2025-07-09 15:51:30 +0530

    string_basic.c

---
+++ string_basic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  // char arr[5]={'a','t','l','a','s'};
+  // printf("%c",arr[1]);
+  char arr[]={'h','e','l','l','o','!'};
+  int i=0;
+  while(arr[i]!='!'){
+      printf("%c",arr[i]);
+      i++;
+  }
+    return 0;
+}
