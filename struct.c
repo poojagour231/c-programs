a64dbf72aa80f0e5b0eef41855e629377ad9162b
Pooja gour <poojagour231@gmail.com>
2025-07-09 17:52:58 +0530

    struct.c

---
+++ struct.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    struct pokemon{
+        int hp;
+        int speed;
+        int attack;
+    };
+    struct pokemon pikachu;
+    pikachu.attack=60;
+    pikachu.hp=50;
+    pikachu.speed=100;
+    printf("%d",pikachu.attack);
+    
+    
+
+    return 0;
+}
