fa483d44797c4e08107c54339a5eebbd78dd6d03
Pooja gour <poojagour231@gmail.com>
2025-07-09 16:16:19 +0530

    C io_string.c

---
+++ i/i/io_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+int main() {
+ 
+  //char str[]="collegeWallah";
+ // printf("%s",str);
+  //printf("\n");
+  //puts("hello everyone");
+  char str[40];
+  gets(str);//entire sentence can be input
+  printf("your input was:%s",str);
+  
+    return 0;
+}
